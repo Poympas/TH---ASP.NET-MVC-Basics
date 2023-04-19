71d8196bde38185538b8bcc4ce1a0c62b57cb552
v.psyharis <v.psyharis@PRM-NB-DEV-13>
2023-04-19 11:15:12 +0300

    Added comic books controller.

---
+++ Comic Book Gallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;   // System.Web.Mvc.Controller
+
+namespace Comic_Book_Gallery.Controllers
+{
+    // Controller classes/files must end with the word Controller
+    public class ComicBooksController : Controller
+    {
+
+    }
+}
