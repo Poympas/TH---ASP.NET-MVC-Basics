4ae6fffde8d5a8d333f1618eab386d59f619aba3
v.psyharis <v.psyharis@PRM-NB-DEV-13>
2023-04-19 11:25:42 +0300

    Controller method and access with url.

--- Comic Book Gallery/Controllers/ComicBooksController.cs
+++ Comic Book Gallery/Controllers/ComicBooksController.cs
@@ -6,9 +6,17 @@ using System.Web.Mvc;   // System.Web.Mvc.Controller
 
 namespace Comic_Book_Gallery.Controllers
 {
-    // Controller classes/files must end with the word Controller
+    /*
+        - Controller classes/files must end with the word Controller.
+        - The class and method must be public to be accessed by the browser.
+        - Controller page can be accessed via url https://localhost:44355/ComicBooks/Detail
+        -                                         https://localhost:xxxxx/CntrlrName/MethodName
+     */
     public class ComicBooksController : Controller
     {
-
+        public string Detail()
+        {
+            return "This is a test.\n I will be gone soon.\n Appriciate me while I'm here. \n Would you kindly?";
+        }
     }
 }
