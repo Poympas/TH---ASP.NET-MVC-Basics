e1628c56aec28dbd523e252763c79cfb4dd60b2a
v.psyharis <v.psyharis@PRM-NB-DEV-13>
2023-04-19 16:20:42 +0300

    Using Comic repository.

--- Comic Book Gallery/Controllers/ComicBooksController.cs
+++ Comic Book Gallery/Controllers/ComicBooksController.cs
@@ -1,13 +1,16 @@
 ﻿/*
+
+
     - Controller classes/files must end with the word Controller.
     - The class and method must be public to be accessed by the browser.
-    - Controller page can be accessed via url https://localhost:44355/ComicBooks/Detail
-                                                https://localhost:xxxxx/CntrlrName/MethodName
+    - Controller page can be accessed via url https://localhost:44355/ComicBooks/Detail/Id
+                                              https://localhost:xxxxx/CntrlrName/MthdName/Id (id is optional)
     - Controller methods are called actions.    
 
 
     - URL routing = url to controller action.
     - Many websites can be hosted on the same machine using different port numbers.
+    - 
 
 
     - ActionResult is mom class of RedirectResult, ContentResult and ViewResult.
@@ -21,7 +24,18 @@
     - ViewBag provides is a dynamic for this communication.
 
 
-    - ViewBag vs Strongly typed view
+    - ViewBag vs Strongly typed view.
+
+
+    - int? makes int nullable.
+
+    
+    - HttpNotFound() returns an action result
+
+
+    - Using the Debugger.
+    - Step into/over/out.
+
 */
 
 using System;
@@ -29,28 +43,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;   // System.Web.Mvc.Controller
+using Comic_Book_Gallery.Data;
 using Comic_Book_Gallery.Models;
 
 namespace Comic_Book_Gallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
         {
-            var comicBook = new ComicBook()
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
             {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() { Name = "Dan Slott", Role = "Script" },
-                    new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
-                    new Artist() { Name = "Victor Olazaba", Role = "Inks" },
-                    new Artist() { Name = "Edgar Delgado", Role = "Colors" },
-                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters" }
-                }
-            };
+                return HttpNotFound();
+            }
+
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
 
             return View(comicBook);
 
