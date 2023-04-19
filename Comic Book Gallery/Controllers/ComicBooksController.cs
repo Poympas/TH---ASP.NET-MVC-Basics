04af5474f8687b595a3b64a4e903c6ef30577a90
v.psyharis <v.psyharis@PRM-NB-DEV-13>
2023-04-19 11:47:09 +0300

    Updated Detail, ActionResult mom class, Redirect(), Content()

--- Comic Book Gallery/Controllers/ComicBooksController.cs
+++ Comic Book Gallery/Controllers/ComicBooksController.cs
@@ -10,13 +10,28 @@ namespace Comic_Book_Gallery.Controllers
         - Controller classes/files must end with the word Controller.
         - The class and method must be public to be accessed by the browser.
         - Controller page can be accessed via url https://localhost:44355/ComicBooks/Detail
-        -                                         https://localhost:xxxxx/CntrlrName/MethodName
+                                                  https://localhost:xxxxx/CntrlrName/MethodName
+        - Controller methods are called actions.    
+
+
+        - URL routing = url to controller action.
+        - Many websites can be hosted on the same machine using different port numbers.
+
+
+        - ActionResult is mom class of RedirectResult and ContentResult.
+        - Controller base class provides methods Content(string) and Redirect(string)
      */
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "This is a test.\n I will be gone soon.\n Appriciate me while I'm here. \n Would you kindly?";
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return Redirect("/");
+            }
+
+            return Content("This is a test.\n I will be gone soon.\n Appriciate me while I'm here. \n Would you kindly?");
+
         }
     }
 }
